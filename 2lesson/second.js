7f645b2d6a868e3cb1ef9615ebda27a559e432a6
Zarinaa14 <smal_david@mail.ru>
2018-03-01 19:11:08 +0200

    a
    a

---
+++ 2lesson/second.js
@@ -0,0 +1,12 @@
+var arr = [3,2,5,6,9];
+function arraySum(array){
+var sum = 0;
+for(var i = 0; i < array.length; i++){
+    if (array[i]%3==0){
+    sum += array[i];}
+	
+	
+    }
+console.log(sum);
+}
+arraySum(arr);
