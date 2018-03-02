b9964cffea6c4b8a232a514d18b525c156874b0a
Zarinaa14 <smal_david@mail.ru>
2018-03-02 21:47:26 +0200

    a
    a

---
+++ 2lesson/first.js
@@ -0,0 +1,39 @@
+var a =  String(prompt("Enter the first value: ")).toLowerCase(); 
+var b =  String(prompt("Enter the second value: ")).toLowerCase(); 
+
+function  Anogramma(a,b) {  
+          if    (a.length == b.length){
+             var arr1 = a.split('');
+			 arr1.sort();
+//             console.log(arr1);
+             var arr2 = b.split('');
+			  arr2.sort();
+//             console.log(arr2);
+			 for( var i = 0; i < arr1.length; i++ ){
+                 for( var j = 0; j < arr2.length; j++ ){
+
+                 if(arr1[i] == arr2[j]){
+
+                    console.log("Анограмма");
+				   return false;
+		            
+				
+			       }
+				    
+				   
+
+                 else {console.log(" не Анограмма");
+				  return false;
+			     }
+			  
+		  
+           }
+		  }	
+        }
+		
+           else 
+           console.log("не Анограмма "); 
+		   return false;
+} 
+Anogramma(a,b);
+ //arr1.join(a);
